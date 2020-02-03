4c92ac2f46f0cc6f7e7adf9d57396248ee872979
Asukin7 <Asukin7@163.com>
2020-02-03 23:06:39 +0800

    AN update-8-bookkeeping-wxml-js-dataListDisplay-dataDeleteButton

--- onStage/pages/bookkeeping/bookkeeping.js
+++ onStage/pages/bookkeeping/bookkeeping.js
@@ -1,11 +1,19 @@
 // pages/bookkeeping/bookkeeping.js
+var  app  =  getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    bkData:{
+      bookkeepingList: [],
+      totalIncome: 0,
+      totalExpend: 0,
+      sumIncomeMoney: 0.00,
+      sumExpendMoney: 0.00
+    }
   },
 
   /**
@@ -19,14 +27,59 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    var that = this;
+    setTimeout(function () {
+      if (app.globalData) {
+        app.getRequest("bookkeeping/listAll", {})
+          .then((res) => {
+            console.log(res)
+            if (res.data.code != 200) {
+              console.log("无法获取数据")
+            }
+            else {
+              that.setData({
+                bkData: {
+                  bookkeepingList: res.data.data.bookkeepingList,
+                  totalIncome: res.data.data.totalIncome,
+                  totalExpend: res.data.data.totalExpend,
+                  sumIncomeMoney: res.data.data.sumIncomeMoney,
+                  sumExpendMoney: res.data.data.sumExpendMoney
+                }
+              })
+            }
+          })
+      } else {
+        console.log("用户未登录！")
+      }
+    }, 2000) 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    if (app.globalData) {
+      app.getRequest("bookkeeping/listAll", {})
+        .then((res) => {
+          console.log(res)
+          if (res.data.code != 200) {
+            console.log("无法获取数据")
+          }
+          else {
+            this.setData({
+              bkData: {
+                bookkeepingList: res.data.data.bookkeepingList,
+                totalIncome: res.data.data.totalIncome,
+                totalExpend: res.data.data.totalExpend,
+                sumIncomeMoney: res.data.data.sumIncomeMoney,
+                sumExpendMoney: res.data.data.sumExpendMoney
+              }
+            })
+          }
+        })
+    } else {
+      console.log("用户未登录！")
+    }
   },
 
   /**
@@ -69,6 +122,26 @@ Page({
     wx.navigateTo({
       url: '../bookkeepingEdit/bookkeepingEdit',
     })
+  },
+
+  deleteBookkeeping(event) {
+    if (app.globalData) {
+      app.getRequest("bookkeeping/deleteBookkeeping", this.data.bkData.bookkeepingList[event.currentTarget.dataset.value])
+        .then((res) => {
+          console.log(res)
+          if (res.data.code != 200) {
+            console.log("删除失败")
+          }
+          else {
+            // console.log(this.data.bkData.bookkeepingList)
+            // this.data.bkData.bookkeepingList.splice([event.currentTarget.dataset.value], 1);//删是删了数组里的数据，但是不会刷新列表，没鬼用
+            // console.log(this.data.bkData.bookkeepingList)
+            console.log("删除成功")
+          }
+        })
+    } else {
+      console.log("用户未登录！")
+    }
   },
 
   // ListTouch触摸开始
